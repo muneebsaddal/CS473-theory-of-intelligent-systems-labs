68e05985cb47c3ac06613031ec898dd9d9e67115
Muneeb Saddal <muneebsaddal@outlook.com>
2019-09-18 23:31:48 +0500

    lab 2 complete!

--- lab 2/or_model.py
+++ lab 2/or_model.py
@@ -3,14 +3,14 @@ from keras.models import Sequential
 from keras.layers.core import Dense
 
 # input arrays
-training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
-target_data = np.array([[0],[1],[1],[0]], "float32")
+training_data = np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]], "float32")
+target_data = np.array([[0],[1],[1],[1],[1],[1],[1],[1]], "float32")
 
-# building xor gate model
+# building or gate model
 model = Sequential()
-model.add(Dense(2, input_dim=2, activation='tanh'))
+model.add(Dense(2, input_dim=3, activation='tanh'))
 model.add(Dense(1, activation='sigmoid'))
-model.summary()
+
 # Compiling model
 model.compile(loss='mean_squared_error',
               optimizer='adam',
